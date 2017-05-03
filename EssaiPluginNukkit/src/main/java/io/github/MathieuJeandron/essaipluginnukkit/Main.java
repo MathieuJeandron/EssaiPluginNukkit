e312af3214c68dc40a363c05c0d579fc4a47c6f4
MathieuJeandron <mathieu.jeandron@gmail.com>
2017-05-03 23:28:14 +0200

    C'est un essai de commit

---
+++ EssaiPluginNukkit/src/main/java/io/github/MathieuJeandron/essaipluginnukkit/Main.java
@@ -0,0 +1,29 @@
+package io.github.MathieuJeandron.essaipluginnukkit;
+
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.Command;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
+import cn.nukkit.event.Listener;
+
+// Plein d'infos sur http://bukkit.gamepedia.com/Plugin_Tutorial
+// A voir comment on "commit" sur Github ? A poursuivre : aide d'éclipse / e-git !
+// on va faire des changements
+
+public class Main extends PluginBase {
+
+  public void onEnable(){
+    this.getLogger().info("My First Plugin For Nukkit Was Loaded");
+  }
+
+  @Override
+      public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getName()) {
+            case "nuke":
+        sender.sendMessage(TextFormat.RED + "FactionsNuke");
+    }
+
+    return true;
+  }
+
+}
